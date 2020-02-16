d2f1f3f45d8f609e98824f7e14715e745c0ab572
Daniel Mackay <daniel.mackay@live.com>
2020-02-16 11:23:59 +1000

    Added part one and two of unit testing with xUnit.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,4 +2,8 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 
-import "prismjs/themes/prism.css"
+//import "prismjs/themes/prism.css"
+//import "prismjs/themes/prism-okaidia.css"
+import "prismjs/themes/prism-tomorrow.css"
+import "prismjs/plugins/command-line/prism-command-line.css"
+//import "./src/styles/prismjs-code-highlighting.css"
